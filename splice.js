b897acd5e829a5ee574b79c1d48899a6b81ed22c
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-08-13 19:54:56 +0530

    Add files via upload

---
+++ splice.js
@@ -0,0 +1,11 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// // At position 2, add 2 elements:
+// fruits.splice(2, 1, "Lemon", "Kiwi");
+// console.log(fruits)
+
+
+
+const name=["manvi","puja","neha","chandani"]
+name.splice(1,2,"tina")
+console.log(name)
